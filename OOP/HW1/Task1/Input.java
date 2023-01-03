fd5567749fdc70f2fbf6124ad253b5524456345e
Mihael Bun <bunmihail@gmail.com>
2023-01-03 09:23:59 +0300

    start hw1

---
+++ OOP/HW1/Task1/Input.java
@@ -0,0 +1,25 @@
+package OOP.HW1.Task1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static int getInputData(Scanner input) {
+        int number = 0;
+        boolean check = false;
+
+        while (!check) {
+            while (!input.hasNextInt()) {
+                System.out.println("Вы ввели не число, повторите ввод!");
+                input.next();
+            }
+            number = input.nextInt();
+
+            if (number <= 0)
+                System.out.println("Вы ввели не натуральное число или число вне диапазона, повторите ввод!");
+            else
+                check = true;
+        }
+
+        return number;
+    }
+}
---
+++ OOP/HW1/Task1/Main.java
@@ -0,0 +1,9 @@
+package OOP.HW1.Task1;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Controller.runPhonebook();
+    }
+}
